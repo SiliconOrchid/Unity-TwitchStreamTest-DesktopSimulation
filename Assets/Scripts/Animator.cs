6d64d93ab24cffcc04aeb0daab87c8d2c97d9be4
Jim McG <jimmcgowan.uk@gmail.com>
2020-05-28 09:04:05 +0100

    initial project drop

---
+++ Assets/Scripts/Animator.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animator : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject _backgroundWindowBox;
+
+    [SerializeField]
+    public GameObject _codeBox;
+
+    [SerializeField]
+    public Camera _camera;
+
+    private AppStateEnum currentAppState;
+
+    private Vector3 leftPosition = new Vector3(-500, 10, -10);
+    private Vector3 rightPosition = new Vector3(500, 10, -10);
+
+
+
+    private enum AppStateEnum
+    {
+        Pausing,
+        FirstMove,
+        SecondMove,
+        ThirdMove,
+        FourthMove,
+        Dormant
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Reset();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateRepositionMovingBox();
+        UpdateClickHandler();
+    }
+
+    private void Reset()
+    {
+        currentAppState = AppStateEnum.Pausing;
+        _codeBox.transform.position = leftPosition;
+        StartCoroutine(PauserCoroutine());
+    }
+
+
+    private void UpdateRepositionMovingBox()
+    {
+
+            switch (currentAppState)
+            {
+                case AppStateEnum.Pausing:
+                {
+                    break;
+                }
+
+                case AppStateEnum.FirstMove:
+                {
+                    _codeBox.transform.position = Vector3.MoveTowards(_codeBox.transform.position, rightPosition, 300 * Time.deltaTime);
+
+                    if (_codeBox.transform.position.x == rightPosition.x)
+                    {
+                        currentAppState = AppStateEnum.SecondMove;
+                    }
+
+                    break;
+                }
+
+                case AppStateEnum.SecondMove:
+                {
+                    _codeBox.transform.position = Vector3.MoveTowards(_codeBox.transform.position, leftPosition, 3000 * Time.deltaTime);
+
+                    if (_codeBox.transform.position.x == leftPosition.x)
+                    {
+                        currentAppState = AppStateEnum.ThirdMove;
+                    }
+
+                    break;
+                }
+
+            case AppStateEnum.ThirdMove:
+                {
+                    _codeBox.transform.position = Vector3.MoveTowards(_codeBox.transform.position, rightPosition, 800 * Time.deltaTime);
+
+                    if (_codeBox.transform.position.x == rightPosition.x)
+                    {
+                        currentAppState = AppStateEnum.FourthMove;
+                    }
+
+                    break;
+                }
+
+            case AppStateEnum.FourthMove:
+                {
+                    _codeBox.transform.position = Vector3.MoveTowards(_codeBox.transform.position, leftPosition, 2000 * Time.deltaTime);
+
+                    if (_codeBox.transform.position.x == leftPosition.x)
+                    {
+                        currentAppState = AppStateEnum.Dormant;
+                    }
+
+                    break;
+                }
+
+
+
+            case AppStateEnum.Dormant:
+
+                    break;
+
+            }
+
+    }
+
+
+
+    private void UpdateClickHandler()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Mouse clicked");
+            Reset();
+        }
+    }
+
+
+    IEnumerator PauserCoroutine()
+    {
+        yield return new WaitForSeconds(2);
+        currentAppState = AppStateEnum.FirstMove;
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+# Unity : TwitchStreamTest - DesktopSimulation
+
+Solution in this repo was used to generate test videos found in my blogsite:
+
+* [Impact of stream quality for live coders](https://blogs.siliconorchid.com/post/coding-inspiration/streaming-livecoders/)
+
+<hr/>
+
+## What are we doing?
+
+I didn't have the time/inclination to learn whether I could move actual windows around a Win10 desktop programatically ... so I used Unity to quickly put together a "fake windows" program.
+
+This animates a "window" of code (cube with a texture) from side to side.
+
+This was used to provide precise repeated movements for capturing a bunch of videos found in the blog linked above.
+
+## Note
+
+I've included the compiled Windows version of the "game" in the subfolder `WindowAnimatorBuild`, meaning you don't need to install Unity to run the same test.
+
+
